d3381f4f5bbe37fd8735f59485b5cc992d781671
Ramon Brand <ramon2009@gmail.com>
2022-05-22 14:12:47 +0300

    Add multisampling

--- src/main.rs
+++ src/main.rs
@@ -3,110 +3,118 @@ use std::sync::Arc;
 use vulkano::{
     buffer::{BufferUsage, CpuAccessibleBuffer},
     command_buffer::{
-        AutoCommandBufferBuilder, CommandBufferUsage, PrimaryAutoCommandBuffer,
-        PrimaryCommandBuffer, SubpassContents,
+        AutoCommandBufferBuilder, CommandBufferUsage, PrimaryAutoCommandBuffer, SubpassContents,
     },
     device::{
         physical::PhysicalDevice, Device, DeviceCreateInfo, DeviceExtensions, Queue,
         QueueCreateInfo,
     },
     format::Format,
-    image::{view::ImageView, ImageUsage, SwapchainImage},
+    image::{view::ImageView, AttachmentImage, ImageUsage, SampleCount, SwapchainImage},
     impl_vertex,
-    instance::{Instance, InstanceCreateInfo, InstanceExtensions},
+    instance::{Instance, InstanceCreateInfo},
     pipeline::{
         graphics::{
             input_assembly::InputAssemblyState,
-            vertex_input::{BuffersDefinition, VertexInputState},
+            vertex_input::BuffersDefinition,
             viewport::{Viewport, ViewportState},
         },
-        GraphicsPipeline, Pipeline,
+        GraphicsPipeline,
     },
     render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass, Subpass},
     single_pass_renderpass,
-    swapchain::{acquire_next_image, Surface, SurfaceCapabilities, Swapchain, SwapchainCreateInfo},
+    swapchain::{acquire_next_image, Surface, Swapchain, SwapchainCreateInfo},
     sync::{now, GpuFuture},
 };
-use vulkano_win::VkSurfaceBuild;
+use vulkano_win::{required_extensions, VkSurfaceBuild};
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
 
-fn setup_instance() -> Arc<Instance> {
-    let instance = Instance::new(InstanceCreateInfo {
-        enabled_extensions: InstanceExtensions {
-            khr_get_physical_device_properties2: true,
-            khr_surface: true,
-            mvk_macos_surface: true,
-            ..InstanceExtensions::none()
-        },
-        ..Default::default()
+fn create_instance() -> Arc<Instance> {
+    let required_extensions = required_extensions();
+    Instance::new(InstanceCreateInfo {
+        enabled_extensions: required_extensions,
+        ..InstanceCreateInfo::default()
     })
-    .expect("Could not create instance");
-    instance
+    .expect("Could not create instance")
 }
 
-fn setup_surface(instance: Arc<Instance>, event_loop: &EventLoop<()>) -> Arc<Surface<Window>> {
-    let surface = WindowBuilder::new()
+fn create_surface(instance: Arc<Instance>, event_loop: &EventLoop<()>) -> Arc<Surface<Window>> {
+    WindowBuilder::new()
         .with_title("My Vulkan Window")
         .build_vk_surface(event_loop, instance)
-        .expect("Unable to create window");
-    surface
+        .expect("Unable to create window")
 }
 
-fn setup_logical_device_and_queue(instance: Arc<Instance>) -> (Arc<Device>, Arc<Queue>) {
+fn create_device_and_queue(
+    instance: Arc<Instance>,
+    surface: Arc<Surface<Window>>,
+) -> (Arc<Device>, Arc<Queue>) {
+    let required_extensions = DeviceExtensions {
+        khr_portability_subset: true,
+        khr_swapchain: true,
+        ..DeviceExtensions::none()
+    };
     let physical_device = PhysicalDevice::enumerate(&instance)
-        .next()
+        .find(|physical_device| {
+            physical_device
+                .supported_extensions()
+                .intersection(&required_extensions)
+                == required_extensions
+                && physical_device
+                    .surface_capabilities(surface.as_ref(), Default::default())
+                    .expect("Could not get surface capabilities")
+                    .supported_usage_flags
+                    .color_attachment
+        })
         .expect("Could not find physical device");
     let queue_family = physical_device
         .queue_families()
         .find(|queue_family| queue_family.supports_graphics())
         .expect("Could not find queue family which supports graphics");
-    let (logical_device, mut queues) = Device::new(
+    let (device, mut queues) = Device::new(
         physical_device,
         DeviceCreateInfo {
-            enabled_extensions: DeviceExtensions {
-                khr_portability_subset: true,
-                khr_swapchain: true,
-                ..DeviceExtensions::none()
-            },
+            enabled_extensions: required_extensions,
             queue_create_infos: vec![QueueCreateInfo::family(queue_family)],
             ..Default::default()
         },
     )
     .expect("Could not create logical device");
     let queue = queues.next().expect("Could not get first queue");
-    (logical_device, queue)
+    (device, queue)
 }
 
-fn setup_swapchain_and_images(
-    logical_device: Arc<Device>,
+fn create_swapchain_and_images(
+    device: Arc<Device>,
     surface: Arc<Surface<Window>>,
 ) -> (Arc<Swapchain<Window>>, Vec<Arc<SwapchainImage<Window>>>) {
-    // Just for demonstration purposes on querying capabilities
-    //let capabilities = logical_device
-    //.physical_device()
-    //.surface_capabilities(&surface, Default::default());
-    //println!("{:?}", capabilities);
     let image_usage = ImageUsage {
         color_attachment: true,
         ..ImageUsage::none()
     };
     let swapchain_create_info = SwapchainCreateInfo {
-        image_usage: image_usage,
+        image_usage,
         ..SwapchainCreateInfo::default()
     };
-    let (swapchain, images) = Swapchain::new(logical_device, surface, swapchain_create_info)
-        .expect("Could not create swapchain");
+    let (swapchain, images) =
+        Swapchain::new(device, surface, swapchain_create_info).expect("Could not create swapchain");
     (swapchain, images)
 }
 
-fn setup_render_pass(logical_device: Arc<Device>) -> Arc<RenderPass> {
-    single_pass_renderpass!(logical_device,
+fn create_render_pass(device: Arc<Device>) -> Arc<RenderPass> {
+    single_pass_renderpass!(device,
                             attachments: {
                                 color: {
+                                    load: Clear,
+                                    store: DontCare,
+                                    format: Format::B8G8R8A8_UNORM,
+                                    samples: 4,
+                                },
+                                output: {
                                     load: Clear,
                                     store: Store,
                                     format: Format::B8G8R8A8_UNORM,
@@ -115,15 +123,16 @@ fn setup_render_pass(logical_device: Arc<Device>) -> Arc<RenderPass> {
                             },
                             pass: {
                                 color: [color],
-                                depth_stencil: {}
+                                depth_stencil: {},
+                                resolve: [output],
                             }
     )
     .unwrap()
 }
 
-fn setup_graphics_pipeline(
-    logical_device: Arc<Device>,
-    swap_chain_extent: [u32; 2],
+fn create_graphics_pipeline(
+    device: Arc<Device>,
+    swapchain_extent: [u32; 2],
     render_pass: Arc<RenderPass>,
 ) -> Arc<GraphicsPipeline> {
     mod vertex_shader {
@@ -140,7 +149,6 @@ fn setup_graphics_pipeline(
                     "
         }
     }
-
     mod fragment_shader {
         vulkano_shaders::shader! {
             ty: "fragment",
@@ -155,18 +163,15 @@ fn setup_graphics_pipeline(
                     "
         }
     }
-
     let vertex_shader_module =
-        vertex_shader::load(logical_device.clone()).expect("Could not load vertex shader");
+        vertex_shader::load(device.clone()).expect("Could not load vertex shader");
     let fragment_shader_module =
-        fragment_shader::load(logical_device.clone()).expect("Could not load fragment shader");
-
+        fragment_shader::load(device.clone()).expect("Could not load fragment shader");
     let viewport = Viewport {
         origin: [0.0, 0.0],
-        dimensions: [swap_chain_extent[0] as f32, swap_chain_extent[1] as f32],
+        dimensions: [swapchain_extent[0] as f32, swapchain_extent[1] as f32],
         depth_range: 0.0..1.0,
     };
-
     let pipeline_builder = GraphicsPipeline::start()
         .vertex_input_state(BuffersDefinition::new().vertex::<Vertex>())
         .input_assembly_state(InputAssemblyState::default())
@@ -187,25 +192,35 @@ fn setup_graphics_pipeline(
             Subpass::from(render_pass.clone(), 0)
                 .expect("Could not create subpass from render pass"),
         );
-
     pipeline_builder
-        .build(logical_device.clone())
+        .build(device.clone())
         .expect("Could not build graphics pipeline")
 }
 
-fn setup_framebuffers(
-    images: &Vec<Arc<SwapchainImage<Window>>>,
+fn create_frame_buffers(
+    device: Arc<Device>,
+    swapchain_images: &Vec<Arc<SwapchainImage<Window>>>,
     render_pass: Arc<RenderPass>,
+    dimenstions: [u32; 2],
 ) -> Vec<Arc<Framebuffer>> {
-    images
+    let color_image = AttachmentImage::multisampled(
+        device.clone(),
+        dimenstions,
+        SampleCount::Sample4,
+        Format::B8G8R8A8_UNORM,
+    )
+    .expect("Could not create color image");
+    let color_image_view =
+        ImageView::new_default(color_image.clone()).expect("Could not create color image view");
+    swapchain_images
         .iter()
         .map(|image| {
-            let image_view =
+            let swapchain_image_view =
                 ImageView::new_default(image.clone()).expect("Could not create image view");
             Framebuffer::new(
                 render_pass.clone(),
                 FramebufferCreateInfo {
-                    attachments: vec![image_view],
+                    attachments: vec![color_image_view.clone(), swapchain_image_view],
                     ..FramebufferCreateInfo::default()
                 },
             )
@@ -214,28 +229,27 @@ fn setup_framebuffers(
         .collect()
 }
 
-fn setup_command_buffers(
-    logical_device: &Arc<Device>,
+fn create_command_buffers(
+    device: &Arc<Device>,
     queue: &Arc<Queue>,
-    framebuffers: &Vec<Arc<Framebuffer>>,
+    frame_buffers: &Vec<Arc<Framebuffer>>,
     graphics_pipeline: &Arc<GraphicsPipeline>,
     vertex_buffer: &Arc<CpuAccessibleBuffer<[Vertex]>>,
 ) -> Vec<Arc<PrimaryAutoCommandBuffer>> {
-    framebuffers
+    frame_buffers
         .iter()
         .map(|framebuffer| {
             let mut command_buffer_builder = AutoCommandBufferBuilder::primary(
-                logical_device.clone(),
+                device.clone(),
                 queue.family(),
                 CommandBufferUsage::OneTimeSubmit,
             )
             .expect("Could not create command buffer");
-
             command_buffer_builder
                 .begin_render_pass(
                     framebuffer.clone(),
                     SubpassContents::Inline,
-                    vec![[1.0, 1.0, 1.0, 1.0].into()],
+                    vec![[1.0, 1.0, 1.0, 1.0].into(), [0.0, 0.0, 0.0, 1.0].into()],
                 )
                 .expect("Could not begin render pass")
                 .bind_pipeline_graphics(graphics_pipeline.clone())
@@ -244,7 +258,6 @@ fn setup_command_buffers(
                 .expect("Could not draw")
                 .end_render_pass()
                 .expect("Could not end render pass");
-
             let command_buffer = command_buffer_builder
                 .build()
                 .expect("Could not build command buffer");
@@ -261,9 +274,9 @@ struct Vertex {
 }
 impl_vertex!(Vertex, position);
 
-fn setup_vertexbuffer(logical_device: Arc<Device>) -> Arc<CpuAccessibleBuffer<[Vertex]>> {
+fn create_vertex_buffer(device: Arc<Device>) -> Arc<CpuAccessibleBuffer<[Vertex]>> {
     CpuAccessibleBuffer::from_iter(
-        logical_device,
+        device,
         BufferUsage::vertex_buffer(),
         false,
         vec![
@@ -283,7 +296,7 @@ fn setup_vertexbuffer(logical_device: Arc<Device>) -> Arc<CpuAccessibleBuffer<[V
 }
 
 fn main_loop(event_loop: EventLoop<()>, vulkan_connector: VulkanConnector) {
-    let mut previous_frame_end = Some(now(vulkan_connector.logical_device.clone()));
+    let mut previous_frame_end = Some(now(vulkan_connector.device.clone()));
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
         match event {
@@ -297,21 +310,21 @@ fn main_loop(event_loop: EventLoop<()>, vulkan_connector: VulkanConnector) {
                     .expect("Could not get previous frame end")
                     .cleanup_finished();
 
-                let command_buffers = setup_command_buffers(
-                    &vulkan_connector.logical_device,
+                let command_buffers = create_command_buffers(
+                    &vulkan_connector.device,
                     &vulkan_connector.queue,
-                    &vulkan_connector.framebuffers,
+                    &vulkan_connector.frame_buffers,
                     &vulkan_connector.graphics_pipeline,
                     &vulkan_connector.vertex_buffer,
                 );
 
-                let (image_index, is_acquired_image_suboptimal, acquire_future) =
+                let (image_index, _is_acquired_image_suboptimal, acquire_future) =
                     match acquire_next_image(vulkan_connector.swapchain.clone(), None) {
                         Ok(result) => result,
                         Err(e) => panic!("{:?}", e),
                     };
 
-                let execution_future = now(vulkan_connector.logical_device.clone())
+                let execution_future = now(vulkan_connector.device.clone())
                     .join(acquire_future)
                     .then_execute(
                         vulkan_connector.queue.clone(),
@@ -336,33 +349,38 @@ fn main_loop(event_loop: EventLoop<()>, vulkan_connector: VulkanConnector) {
 }
 
 struct VulkanConnector {
-    logical_device: Arc<Device>,
+    device: Arc<Device>,
     queue: Arc<Queue>,
-    framebuffers: Vec<Arc<Framebuffer>>,
+    frame_buffers: Vec<Arc<Framebuffer>>,
     graphics_pipeline: Arc<GraphicsPipeline>,
     swapchain: Arc<Swapchain<Window>>,
     vertex_buffer: Arc<CpuAccessibleBuffer<[Vertex]>>,
 }
 
 fn main() {
-    let instance = setup_instance();
+    let instance = create_instance();
     let event_loop = EventLoop::new();
-    let surface = setup_surface(instance.clone(), &event_loop);
-    let (logical_device, queue) = setup_logical_device_and_queue(instance.clone());
-    let (swapchain, images) = setup_swapchain_and_images(logical_device.clone(), surface.clone());
-    let render_pass = setup_render_pass(logical_device.clone());
-    let graphics_pipeline = setup_graphics_pipeline(
-        logical_device.clone(),
+    let surface = create_surface(instance.clone(), &event_loop);
+    let (device, queue) = create_device_and_queue(instance.clone(), surface.clone());
+    let (swapchain, images) = create_swapchain_and_images(device.clone(), surface.clone());
+    let render_pass = create_render_pass(device.clone());
+    let graphics_pipeline = create_graphics_pipeline(
+        device.clone(),
         swapchain.image_extent(),
         render_pass.clone(),
     );
-    let framebuffers = setup_framebuffers(&images, render_pass.clone());
-    let vertex_buffer = setup_vertexbuffer(logical_device.clone());
+    let frame_buffers = create_frame_buffers(
+        device.clone(),
+        &images,
+        render_pass.clone(),
+        swapchain.image_extent(),
+    );
+    let vertex_buffer = create_vertex_buffer(device.clone());
 
     let vulkan_connector = VulkanConnector {
-        logical_device,
+        device,
         queue,
-        framebuffers,
+        frame_buffers,
         graphics_pipeline,
         swapchain,
         vertex_buffer,
